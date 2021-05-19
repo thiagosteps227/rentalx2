4605d90dd5f89cd155e549c68d4b8857821220f9
thiagosteps227 <thiagosteps@gmail.com>
2021-05-19 11:50:14 +0100

    with specifications table

--- rentalx/src/modules/cars/repositories/implementations/SpecificationsRepositories.ts
+++ rentalx/src/modules/cars/repositories/implementations/SpecificationsRepositories.ts
@@ -1,29 +1,25 @@
+import { getRepository, Repository } from "typeorm";
 import Specification from "../../entities/Specification";
 import { ISpecificationsRepository, ICreateSpecificationDTO } from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
-    private specifications: Specification[];
+    private repository: Repository<Specification>;
 
     constructor() {
-      this.specifications = [];
+      this.repository = getRepository(Specification);
     }
 
-    create({ description, name }: ICreateSpecificationDTO): void {
-      const specification = new Specification();
+    async create({ description, name }: ICreateSpecificationDTO): Promise<void> {
+     const specification = this.repository.create({
+       description,
+       name,
+     })
 
-      Object.assign(specification, {
-        name,
-        description,
-        created_at: new Date(),
-      });
-
-      this.specifications.push(specification);
+     await this.repository.save(specification);
     }
 
-    findByName(name: string): Specification {
-      const specification = this.specifications.find(
-        (specification) => specification.name === name
-        );
+    async findByName(name: string): Promise<Specification> {
+      const specification = this.repository.findOne({ name });
         return specification;
     }
 }
